7b821330e5594f74a84a64874d9c37608ba914e8
Censini <49305495+Censini@users.noreply.github.com>
2025-04-01 13:05:55 +0200

    refactor: suppression des dossiers obsolètes (controllers, routes, utils)

--- src/routes/networks.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { listNetworks } from '../controllers/networks';
-
-export const networksRouter = express.Router();
-
-networksRouter.get('/:organizationId', listNetworks);
